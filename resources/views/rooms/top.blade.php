ee91900fa87f9c98474362186d0723881f3e4689
persecond0392 <persecond0392@toki.waseda.jp>
2022-12-23 22:31:36 +0900

    トップ画面の作成

---
+++ resources/views/rooms/top.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>アプリ名</h1>
+        <div class='body'>
+            <div class='introduction'>
+                <h2>ブレインストーミングとは？</h2>
+            </div>
+            <div class='start'>
+                <a href='/rooms/certification'>始める</a>
+            </div>
+        </div>
+    </body>
+</html>
